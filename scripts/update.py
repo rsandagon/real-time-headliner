329208ae48448909628a699345de6d2ef4f95186
ryan sandagon <rsandagon@gmail.com>
2021-12-01 23:55:43 +0800

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -34,7 +34,7 @@ def get_rappler_headline(url):
     soup = BeautifulSoup(r.text,'html.parser')
     print('>', soup.find('h3'))
     h3 =  soup.find('h3')
-    parent = h3.find_parent()
+    parent = h3
     return {
         "url": url + parent['href'],
         "headline": h3.text
